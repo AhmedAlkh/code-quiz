d7f4cf14ab3471f39344b26582e6bbb84669887c
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-11-13 22:37:41 -0500

    Start quiz functional, loops through questions

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,112 @@
 var startButton = document.querySelector("#start-btn");
-var questions = document.querySelector("#question");
-var time = document.queryselector("#time")
-var correctAnswer
+var questionBox = document.querySelector("#questions-box");
+var questionTitle = document.querySelector("#question");
+var time = document.querySelector("#time")
+var intro = document.querySelector("#introduction");
+var answerButtons = document.querySelector("#answer-buttons");
+var resultBox = document.querySelector("#result-box");
+var result = document.querySelector("#result");
+var correctAnswer;
 var score = 0;
+var counter = 75;
+var currentQuestion = 0;
+
+
 
 // Timer 
-function setTime() {
-    var countDown = setInterval(function () {
-        counter --;
-        if (counter === -1) {
+function setTime() {   
+    counter = 75; 
+    console.log("started");
+    var countDown = setInterval(function() {
+        counter --;        
+        if (counter === -1) {            
             clearInterval(countdown);
         }
-        timer.textcontent = counter; 
+        time.textcontent = counter; 
     }, 1000);
 };
 // Timer ends
 
 function startQuiz() {
     var showQuestions = document.getElementById("#questions");
+    intro.classList.add("hidden");
+    questionBox.classList.remove("hidden");
+    currentQuestion = 0;    
 
     //start timer when first question is revealed
     setTime();
+
+    //show the first question
+    showNextQuestion();
 };
+
+
+
+function showNextQuestion() {    
+    var question = questions[currentQuestion];
+    questionTitle.innerHTML = question.title;
+    console.log("Question #" + currentQuestion);
+    console.log(question.title);
+    question.choices.forEach(function(choice, index) {
+        var button = document.querySelector("#answer" + index);
+        button.innerHTML = choice;     
+        button.setAttribute("correctAnswer", question.correctAnswer);
+        
+        //answerButtons.appendChild(button);
+        
+    });
+    console.log(question);
+}
 
 // Questions, options, answers
 var questions = [
-{
-title: "Commonly used data types do not include:",
-Choices: ["strings", "booleans", "alerts", "numbers",],
-correctAnswer: "alerts",
-},
-{
-title: "The condition in an if / else statement is enclosed with ____ ?",
-Choices: ["quotes", "curly brackets", "parenthesis", "square brackets",],
-correctAnswer: "parenthesis",
-},
-{
-title: "Arrays in JavaScript can be used to store ____ ?",
-Choices: ["Numbers & Strings", "Other arrays", "Booleans", "All of the above",],
-correctAnswer: "All of the above",
-},
-{
-title: "String Values must be enclosed within ____ when being assigned to variables.",
-Choices: ["Commas", "Curly brackets", "Quotes", "Parenthesis",],
-correctAnswer: "Quotes",
-},
-{
-title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-Choices: ["Javascript", "Terminal/Bash", "for loops", "console.log",],
-correctAnswer: "console.log",
-},
+    {
+        title: "Commonly used data types do not include:",
+        choices: ["strings", "booleans", "alerts", "numbers",],
+        correctAnswer: "alerts",
+    },
+    {
+        title: "The condition in an if / else statement is enclosed with ____ ?",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets",],
+        correctAnswer: "parenthesis",
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____ ?",
+        choices: ["Numbers & Strings", "Other arrays", "Booleans", "All of the above",],
+        correctAnswer: "All of the above",
+    },
+    {
+        title: "String Values must be enclosed within ____ when being assigned to variables.",
+        choices: ["Commas", "Curly brackets", "Quotes", "Parenthesis",],
+        correctAnswer: "Quotes",
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "Terminal/Bash", "for loops", "console.log",],
+        correctAnswer: "console.log",
+    },
 ];
+
+startButton.addEventListener("click", startQuiz);
+
+var numberOfAnswers = 4;
+for (i = 0; i < numberOfAnswers; i++) {
+     
+    var button = document.querySelector("#answer" + i);           
+    button.addEventListener("click", function(){     
+        resultBox.classList.remove("hidden");    
+    
+        if (this.getAttribute("correctAnswer") == this.innerHTML) {
+            result.innerHTML = "Correct!";
+        } else {
+            result.innerHTML = "Wrong!";
+            counter -= 10;
+        }
+        currentQuestion++;
+        showNextQuestion();
+    });
+    }
+
 // Questions, options, answers ends
 
 // Local storage
